fdbbe52bdc8b2da82bb878d8692d6893c7e0eb63
HARUKI KONDO <65643010+HarukiKondo@users.noreply.github.com>
2023-05-24 02:28:16 +0000

    テンプレを追記

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Web5-SDK-Sample
 
 ## 参考文献
 1. [Web5 SDK Docs](https://developer.tbd.website/docs/)
+2. [Build a ToDo App](https://developer.tbd.website/docs/web5/build/apps/todo-app-tutorial)
+3. [tutorial Repo](https://github.com/TBD54566975/web5-tutorials)
---
+++ todo-app/src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { onBeforeMount, ref, toRaw } from 'vue';
+import { PlusIcon as PlusIconMini } from '@heroicons/vue/solid';
+import { CheckCircleIcon, TrashIcon } from '@heroicons/vue/outline';
+
+
+onBeforeMount(async () => {
+  // Populate ToDos from DWN
+
+  // Add entry to ToDos array
+
+});
+
+
+const newTodoDescription = ref('');
+
+/**
+ * Adding ToDos method
+ */
+async function addTodo() {
+
+}
+
+/**
+ * Delete ToDo
+ * @param {*} todoItem 
+ */
+async function deleteTodo(todoItem) {
+
+}
+
+/**
+ * Toggling ToDo Status
+ * @param {*} todoItem 
+ */
+async function toggleTodoComplete(todoItem) {
+
+}
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center min-h-full px-8 py-12 sm:px-6">
+      <!-- Title -->
+      <div class="sm:max-w-md sm:w-full">
+          <h2 class="font-bold text-3xl text-center tracking-tight">
+              Todo List
+          </h2>
+      </div>
+      <!-- Add ToDos Form -->
+      <!-- ToDos -->
+  </div>
+</template>
